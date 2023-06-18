82f227538550f35ff5b0d954df00f1eed485c7b5
n3s-online <n3s.online@gmail.com>
2023-06-18 11:14:32 -0700

    remove seconds from most recent card

--- frontend/src/util/dateUtil.ts
+++ frontend/src/util/dateUtil.ts
@@ -1,6 +1,7 @@
 export const getFullDateString = (date: Date) => {
     const dateStringInPst = date.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-    return dateStringInPst;
+    const withoutSeconds = dateStringInPst.substring(0, dateStringInPst.length - 6) + dateStringInPst.substring(dateStringInPst.length - 3);
+    return withoutSeconds;
 }
 
 export const getDateHhMm = (date: Date) => date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
